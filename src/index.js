bdec7f17e2e3e02aba6a6865b1b1e9f4ab835d54
ColinS97 <sgtwigglytuff@gmail.com>
2018-10-26 16:24:16 +0200

    implemented data in memory and added feed and link obejcts

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `
+let links = [{
+    id: 'link-0',
+    url:'www.howtographql.com',
+    description:'Fullstack tutorial for GraphQL'
+}]
 
 //2
 const resolvers = {
     Query: {
+        // cannot put null here graphql-js implementation prevents this mistake because String! forces some input!=null
         info: () => `This is the API of a Hackernew Clone`
     }
 }
