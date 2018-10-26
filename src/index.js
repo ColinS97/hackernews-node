915e20e6c32384da5c459d3366ff7fab06f95af1
ColinS97 <sgtwigglytuff@gmail.com>
2018-10-26 17:10:22 +0200

    externalized schema and added post functionality

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-//1
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
 
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
+//data is stored in links variable
 let links = [{
     id: 'link-0',
     url:'www.howtographql.com',
     description:'Fullstack tutorial for GraphQL'
+},{
+    id: 'link-1',
+    url:'www.howtographql.de',
+    description:'dummy'
 }]
 
-//2
+let idCount = links.length
 const resolvers = {
     Query: {
         // cannot put null here graphql-js implementation prevents this mistake because String! forces some input!=null
-        info: () => `This is the API of a Hackernew Clone`
+        info: () => `This is the API of a Hackernew Clone`,
+        //calling feed resolves to links
+        feed: () => links,
+    },
+
+    Mutation: {
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        }
     }
+    //resolving link is trivial so it can be commented out
+    //Link: {
+    //    id: (root) => root.id,
+    //    description: (root) => root.description,
+    //    url: (root) => root.url,
+    //}
+
 }
 
-//3
+
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs:'./src/schema.graphql',
     resolvers,
 })
 server.start(() =>  console.log(`Server is running on http://localhost:4000`))
